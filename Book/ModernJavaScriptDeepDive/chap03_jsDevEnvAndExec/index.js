594374f22b6352f8bd2e205aef30c3913cabc508
ainochi-kor <kthrkdals@gmail.com>
2021-05-01 13:30:08 +0900

    add Modern js

--- Book/ModernJavaScriptDeepDive/chap03_jsDevEnvAndExec/index.js
+++ Book/ModernJavaScriptDeepDive/chap03_jsDevEnvAndExec/index.js
@@ -1,3 +1,11 @@
 const arr = [1,2,3];
 
-arr.forEach(alert);
+//arr.forEach(alert);
+
+// </ 예제04-10
+console.log(scroe)
+
+score = 80;
+var score;
+//console.log(scroe) // ReferenceError: scroe is not defined 출력
+// />
