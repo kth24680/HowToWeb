714ba29577db21593542e12b240d6a34b048d1ca
ainochi-kor <kthrkdals@gmail.com>
2021-06-04 13:41:51 +0900

    add 꾸미기

--- org/React/TicTacToe/js/main.js
+++ org/React/TicTacToe/js/main.js
@@ -66,21 +66,22 @@ class Board extends React.Component {
     */
     return (
       <div>
-        <div className="board-row">
+        <div className="board-row center-block">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className="board-row center-block">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className="board-row center-block">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <br></br>
       </div>
     );
   }
